0331b57082b51696bcb9d3afaaa06a0f756c7a2f
NaqeNaqka <joankociko1@gmail.com>
2024-12-26 09:20:15 +0100

    added memory limiter (experimental)

--- src/server.py
+++ src/server.py
@@ -150,4 +150,27 @@ async def run_main(request: Request, background_tasks: BackgroundTasks):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    import resource
+    import sys
+
+    def memory_limit_half():
+        """Limit max memory usage to half."""
+        soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+        # Convert KiB to bytes, and divide in two to half
+        resource.setrlimit(resource.RLIMIT_AS, (int(get_memory() * 1024 / 2), hard))
+
+    def get_memory():
+        with open('/proc/meminfo', 'r') as mem:
+            free_memory = 0
+            for i in mem:
+                sline = i.split()
+                if str(sline[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
+                    free_memory += int(sline[1])
+        return free_memory  # KiB
+
+    memory_limit_half()
+    try:
+        uvicorn.run(app, host="0.0.0.0", port=8080)
+    except MemoryError:
+        sys.stderr.write('\n\nERROR: Memory Exception\n')
+        sys.exit(1)
